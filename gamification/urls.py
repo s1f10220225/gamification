4c7a51d225ac3a5b954e7894ed6774a47f305538
1F10220188 <s1f102201885@iniad.org>
2024-09-19 06:54:39 +0900

    ステータス表示画面の追加

--- gamification/urls.py
+++ gamification/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('259', views.quest, name='quest'),
     path('quest/<int:quest_id>/', views.quest_detail, name='quest_detail'),  # 修正
     path('quest/add/', views.add_quest, name='add_quest'),  # クエスト追加用のURLパターン
+    path('user/<int:user_id>/status/', views.display_status, name='display_status'),  # ステータス表示用のURL
 ]
--- gamification/views.py
+++ gamification/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import Http404
-from .models import Quest  # Questモデルをインポート
+from .models import Quest,User,Status  # Quest,User,Statusモデルをインポート
 from .forms import QuestForm  # QuestFormをインポート
 
 def top(request):
@@ -24,3 +24,8 @@ def add_quest(request):
         form = QuestForm()
 
     return render(request, "gamification/259add_quest.html", {'form': form})  # フォームをテンプレートに渡す
+
+def display_status(request, user_id):
+    user = get_object_or_404(User, pk=user_id)  # IDを使ってユーザーを取得
+    status = get_object_or_404(Status, pk=user.user_id)  # 取得したユーザーを使ってステータスを取得
+    return render(request, 'gamification/display_status.html', {'user': user, 'status': status})  # 取得したユーザーをテンプレートに渡す
