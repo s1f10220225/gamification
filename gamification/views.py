996ec61a93c820eefe910251026a94a580f7b6b9
1F10220188 <s1f102201885@iniad.org>
2024-10-29 03:59:26 +0900

    models.pyの更新に合わせたdisplay_status.htmlの変更

--- gamification/views.py
+++ gamification/views.py
@@ -33,7 +33,7 @@ def add_quest(request):
 
 def display_status(request, user_id):
     user = get_object_or_404(User, pk=user_id)  # IDを使ってユーザーを取得
-    status = Status.objects.filter(user=user.user_id)  # 取得したユーザーを使ってステータスを取得
+    status = Status.objects.filter(user=user.user_id).order_by("category")  # 取得したユーザーを使ってステータスを取得
     return render(request, 'gamification/display_status.html', {'user': user, 'status': status})  # 取得したユーザーをテンプレートに渡す
 
 def password(request):
