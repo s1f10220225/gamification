dc3e39f5c5b384b0dbdea611076d2e68bfc6958d
s1f10220259 <107388715+s1f10220259@users.noreply.github.com>
2024-12-04 15:21:10 +0900

    指示を指定する

--- gamification/views.py
+++ gamification/views.py
@@ -186,7 +186,8 @@ def get_gpt_response(api_key, order, user_message, temperature=0.2):
 def sample_return(request):
     if request.method == 'POST':
         api_key = request.POST.get('api_key')
-        order = request.POST.get('order')
+        #order = request.POST.get('order')
+        order = "文章を要約して、必要な資格や能力も書いてほしいまた、必要な能力と文章は改行して分けて"
         user_message = request.POST.get('user_message')
         ans = get_gpt_response(api_key, order, user_message, temperature=0.2)
         return render(request, "gamification/sample.html", {"response": ans})
