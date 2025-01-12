1dcfcbd1d2609601f5877acc01329dd4b6225990
s1f10220224 <s1f102202246@iniad.org>
2025-01-13 03:43:13 +0900

    login

--- gamification/urls.py
+++ gamification/urls.py
@@ -4,27 +4,8 @@ from . import views
 from .views import user_profile 
 
 urlpatterns = [
-    path('259', views.quest, name='quest'),
-    path('quest/<int:quest_id>/', views.quest_detail, name='quest_detail'),  # 修正
-    path('quest/add/', views.add_quest, name='add_quest'),  # クエスト追加用のURLパターン
-    path('user/<int:user_id>/status/', views.display_status, name='display_status'),  # ステータス表示用のURL
-    path('pass/', views.password2, name='password'),  # パスワード入力ページのURL
-    path('quest/accept/<int:quest_id>/', views.accept_quest, name='accept_quest'),  # 受けるURLを追加
-    path('225/gpt', views.gpt, name='gpt'), # GPTと会話できるプロトタイプページ
-    path('party/create/', views.CreatePartyView.as_view(), name='create_party'), # パーティー作成
-    path('party/<int:party_id>/', views.PartyDetailView.as_view(), name='party_detail'), # パーティーの詳細(メンバー一覧)
-    path('party/<int:party_id>/add_member/', views.AddMemberView.as_view(), name='add_member'), # メンバーの追加
-    path('party/<int:party_id>/remove_member/<int:user_id>/', views.RemoveMemberView.as_view(), name='remove_member'), # メンバーの削除
-    path('',views.toppage,name='toppage'),
-
-    
-    path('', views.top, name='top'),
-    path('259/', views.quest, name='quest'),
-    
     path('quest/<int:quest_id>/', views.quest_detail, name='quest_detail'),
-    path('pass/', views.password2, name='password'),
     path('quest/accept/<int:quest_id>/', views.accept_quest, name='accept_quest'),
-    path('225/gpt/', views.gpt, name='gpt'),
     path('user/<int:user_id>/status/', views.display_status, name='display_status'),
     path('user/<int:user_id>/career_to_status/', views.career_to_status, name='career_to_status'),
     path('party/create/', views.CreatePartyView.as_view(), name='create_party'),
@@ -41,4 +22,9 @@ urlpatterns = [
     path('accounts/signup/', views.SignUp.as_view(), name='signup'),
     path('accounts/signup/complete/', views.signup_complete, name='signup_complete'),
     path('accounts/login/', views.LoginView.as_view(), name='login'),
+
+    path('224toppage',views.toppage,name='toppage'),
+    path('224login',views.main_login,name='main_login'),
+
+    
 ]
